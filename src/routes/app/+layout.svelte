6cc172941847ba80d6eedbba67f43e3d4b7b13a7
Brian Beard <brian.a.beard@gmail.com>
2025-01-25 19:50:09 -0600

    add sidebar component boilerplate

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	// Libraries & Modules
+	import type { Snippet } from 'svelte';
+
+	// Components
+	import SidebarPage from '$lib/components/sidebar-page.svelte';
+
+	// Icons
+
+	// Types
+
+	type Props = {
+		data: any;
+		children: Snippet;
+	};
+
+	let { data, children }: Props = $props();
+</script>
+
+<SidebarPage>
+	{@render children?.()}
+</SidebarPage>
